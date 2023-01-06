ca71f5716672c8ed8396507a5b70fe2d628ba3e7
Fredrik Rinsén <Rinsen@users.noreply.github.com>
2023-01-07 00:19:56 +0100

    Adding some chunk tests (#3)
    * Adding some chunk tests
    
    * Working chunks

--- test/Rinsen.Gelf.Tests/Internal/UdpClientMock/UdpTestClient.cs
+++ test/Rinsen.Gelf.Tests/Internal/UdpClientMock/UdpTestClient.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Nodes;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using Rinsen.Gelf.Internal;
+using static Rinsen.Gelf.UdpGelfTransport;
 
 namespace Rinsen.Gelf.Tests.Internal.UdpClientMock
 {
@@ -23,13 +25,80 @@ namespace Rinsen.Gelf.Tests.Internal.UdpClientMock
             return Task.FromResult(0);
         }
 
-        public JsonDocument GetPayload()
+        public JsonDocument? GetPayload()
         {
-            var data = Encoding.UTF8.GetString(_udpDatagrams.First().Datagram);
+            if (!_udpDatagrams.Any())
+            {
+                return null;
+            }
 
-            var result = JsonSerializer.Deserialize<JsonDocument>(data);
+            if (_udpDatagrams.Count == 1)
+            {
+                var data = Encoding.UTF8.GetString(_udpDatagrams.First().Datagram);
 
-            return result;
+                var result = JsonSerializer.Deserialize<JsonDocument>(data);
+
+                return result;
+            }
+
+            var firstMessageDatagram = _udpDatagrams.Single(m => m.Order == 0).Datagram.AsSpan();
+            var firstMessageId = firstMessageDatagram.Slice(2, 8);
+            var firstSequenceCount = firstMessageDatagram[11];
+
+            if (firstSequenceCount != _udpDatagrams.Count)
+            {
+                throw new Exception($"Wrong number of chunks, expected {firstSequenceCount} but recieved {_udpDatagrams.Count}");
+            }
+
+            var messageLength = 0;
+            foreach (var udpDatagram in _udpDatagrams.OrderBy(m => m.Order))
+            {
+                if (udpDatagram.Datagram[GelfChunkHeader.MagicByte1] != GelfChunkHeader.MagicByte1Value || 
+                    udpDatagram.Datagram[GelfChunkHeader.MagicByte2] != GelfChunkHeader.MagicByte2Value)
+                {
+                    throw new Exception("Error in magic bytes");
+                }
+
+                var datagramSpan = udpDatagram.Datagram.AsSpan();
+
+                var messageId = datagramSpan.Slice(2, 8);
+
+                if (!firstMessageId.SequenceEqual(messageId))
+                {
+                    throw new Exception("Message id is not the same in chunk");
+                }
+
+                var sequenseNumber = datagramSpan[10]; // The sequence number of this chunk starts at 0 and is always less than the sequence count.
+                if (sequenseNumber != udpDatagram.Order)
+                {
+                    throw new Exception("Message order is not correct");
+                }
+
+                var sequenseCount = datagramSpan[11]; // Total number of chunks in this message.
+                if (sequenseCount != firstSequenceCount)
+                {
+                    throw new Exception("Message chunk length is not correct");
+                }
+
+                messageLength += udpDatagram.ByteSize - 12;
+            }
+
+            var buffer = new byte[messageLength];
+
+            var count = 0;
+            foreach (var udpDatagram in _udpDatagrams.OrderBy(m => m.Order))
+            {
+                var length = udpDatagram.Datagram.Length - 12;
+
+                Array.Copy(udpDatagram.Datagram, 12, buffer, 8180 * count, length);
+                count++;
+            }
+
+            var chunkedData = Encoding.UTF8.GetString(buffer);
+
+            var chunkedResult = JsonSerializer.Deserialize<JsonDocument>(chunkedData);
+
+            return chunkedResult;
         }
     }
 }
--- test/Rinsen.Gelf.Tests/Internal/UdpGelfTransportTest.cs
+++ test/Rinsen.Gelf.Tests/Internal/UdpGelfTransportTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Rinsen.Gelf.Tests.Internal
 {
     public class UdpGelfTransportTest
     {
+        private readonly string[] _names = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
 
         // Standard description https://go2docs.graylog.org/5-0/getting_in_log_data/gelf.html
         [Fact]
@@ -73,6 +75,100 @@ namespace Rinsen.Gelf.Tests.Internal
                 { "full_message", new ExpectedProperty("This is the complete message", JsonValueKind.String) },
                 { "timestamp", new ExpectedProperty("123", JsonValueKind.Number) },
             });
+        }
+
+        [Fact]
+        public async Task WhenSendingAPayload_WhitTwoChunks_GetTheCorrectPayload()
+        {
+            // Arrange
+            var udpTestClient = new UdpTestClient();
+            var udpGelfTransport = new UdpGelfTransport(udpTestClient);
+            using var cancellationTokenSource = new CancellationTokenSource();
+            var additionalFields = GetAdditionalFields(80);
+            var gelfPayload = new GelfPayload
+            {
+                Level = GelfLogLevel.Emergency,
+                Host = "FakeHost",
+                ShortMessage = "My short message",
+                Timestamp = 123,
+                FullMessage = "This is the complete message",
+                AdditionalFields = additionalFields
+            };
+
+            // Act
+            await udpGelfTransport.Send(gelfPayload, cancellationTokenSource.Token);
+
+            // Asser
+            var data = udpTestClient.GetPayload();
+
+            var propertiesToAssert = new Dictionary<string, ExpectedProperty> {
+                { "version", new ExpectedProperty("1.1", JsonValueKind.String) },
+                { "level", new ExpectedProperty("0", JsonValueKind.Number) },
+                { "host", new ExpectedProperty("FakeHost", JsonValueKind.String) },
+                { "short_message", new ExpectedProperty("My short message", JsonValueKind.String) },
+                { "full_message", new ExpectedProperty("This is the complete message", JsonValueKind.String) },
+                { "timestamp", new ExpectedProperty("123", JsonValueKind.Number) } };
+
+            foreach (var additionalField in additionalFields)
+            {
+                propertiesToAssert.Add(additionalField.Key, new ExpectedProperty(additionalField.Value.ToString(), JsonValueKind.String));
+            }
+
+            AssertProperties(data, propertiesToAssert);
+        }
+
+        [Fact]
+        public async Task WhenSendingAPayload_WhitToManyChunks_GetASilentDropOfPackage()
+        {
+            // Arrange
+            var udpTestClient = new UdpTestClient();
+            var udpGelfTransport = new UdpGelfTransport(udpTestClient);
+            using var cancellationTokenSource = new CancellationTokenSource();
+            var additionalFields = GetAdditionalFields(5600);
+            var gelfPayload = new GelfPayload
+            {
+                Level = GelfLogLevel.Emergency,
+                Host = "FakeHost",
+                ShortMessage = "My short message",
+                Timestamp = 123,
+                FullMessage = "This is the complete message",
+                AdditionalFields = additionalFields
+            };
+
+            // Act
+            await udpGelfTransport.Send(gelfPayload, cancellationTokenSource.Token);
+
+            // Asser
+            var data = udpTestClient.GetPayload();
+
+            Assert.Null(data);
+        }
+
+        private Dictionary<string, object> GetAdditionalFields(int count)
+        {
+            var additionalFields = new Dictionary<string, object>();
+
+            for (int i = 0; i < count; i++)
+            {
+                if (i < 10)
+                {
+                    additionalFields.Add($"_{_names[i]}", $"{_names[i]}_dfsfgdfhFGHfghj fdgh fgh fhd fgghfDHUJKFGyuikugszffd sda fDF gdfgjghk fdsagfgtyu jjkjk vghjasdf gsghsdfghtyuioulkfhdgf  fgs asdfggh dfhjd fhjhjk gf");
+                }
+                else if (i < 100)
+                {
+                    additionalFields.Add($"_{_names[i/10]}{_names[i % 10]}", $"{_names[i / 10]} {_names[i % 10]} dfsfgdfhFGHfghj fdgh fgh fhd fgghfDHUJKFGyuikugszffd sda fDF gdfgjghk fdsagfgtyu jjkjk vghjasdf gsghsdfghtyuioulkfhdgf  fgs asdfggh dfhjd fhjhjk gf");
+                }
+                else if (i < 1000)
+                {
+                    additionalFields.Add($"_{_names[i/100]}{_names[i%100/10]}{_names[i%10]}", $"{_names[i / 100]} {_names[i % 100 / 10]} {_names[i % 10]} dfsfgdfhFGHfghj fdgh fgh fhd fgghfDHUJKFGyuikugszffd sda fDF gdfgjghk fdsagfgtyu jjkjk vghjasdf gsghsdfghtyuioulkfhdgf  fgs asdfggh dfhjd fhjhjk gf");
+                }
+                else if (i < 10000)
+                {
+                    additionalFields.Add($"_{_names[i/1000]}{_names[i%1000/100]}{_names[i/10%10]}{_names[i%10]}", $"{_names[i / 1000]} {_names[i % 1000 / 100]} {_names[i / 10 % 10]} {_names[i % 10]} dfsfgdfhFGHfghj fdgh fgh fhd fgghfDHUJKFGyuikugszffd sda fDF gdfgjghk fdsagfgtyu jjkjk vghjasdf gsghsdfghtyuioulkfhdgf  fgs asdfggh dfhjd fhjhjk gf");
+                }
+            }
+
+            return additionalFields;
         }
 
         private static void AssertProperties(JsonDocument data, Dictionary<string, ExpectedProperty> expectedProperties)
@@ -148,5 +244,7 @@ namespace Rinsen.Gelf.Tests.Internal
                 Exists = true;
             }
         }
+
+        
     }
 }
